de40df6dab9415cc0c291fded8934c2d74591306
Lucas Brown <lucasbrown@lucasbrownish.com>
2020-07-02 18:53:19 -0500

    Add macOS implementation and fix compile tests

--- Sources/SwiftUIVisualEffects/View Modifiers/VibrancyEffectModifier.swift
+++ Sources/SwiftUIVisualEffects/View Modifiers/VibrancyEffectModifier.swift
@@ -2,20 +2,27 @@
 * SwiftUIVisualEffects
 */
 
-#if os(iOS)
-
 import SwiftUI
 
 /// Creates a vibrancy effect.
 public struct VibrancyEffectModifier: ViewModifier {
 	public init() {}
 	
+	#if os(iOS)
 	public func body(content: Content) -> some View {
 		content
 			// Hide the original content, keeping its intrinsic content size.
 			.hidden()
 			.overlay(_VibrancyVisualEffectViewRepresentable(content: content))
 	}
+	
+	#elseif os(macOS)
+	public func body(content: Content) -> some View {
+		content
+			// Hide the original content, keeping its intrinsic content size.
+			.hidden()
+			.overlay(_ViewRepresentable(content: content))
+	}
+	
+	#endif
 }
-
-#endif
