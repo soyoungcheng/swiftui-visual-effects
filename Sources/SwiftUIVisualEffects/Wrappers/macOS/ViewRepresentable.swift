1815bdcb41fa115dd9f58f0c77629ebabee912f7
Lucas Brown <lucasbrown@lucasbrownish.com>
2020-07-03 16:47:39 -0500

    Begin implementation

--- Sources/SwiftUIVisualEffects/Wrappers/macOS/ViewRepresentable.swift
+++ Sources/SwiftUIVisualEffects/Wrappers/macOS/ViewRepresentable.swift
@@ -12,12 +12,25 @@ struct _ViewRepresentable<Content: View>: NSViewRepresentable {
 	}
 	
 	func makeNSView(context: Context) -> NSView {
-		NSView()
+		let visualEffectView = NSVisualEffectView()
+		visualEffectView.addSubview(HostingView(rootView: content))
+		visualEffectView.material = context.environment.blurEffectStyle
+		visualEffectView.subviews.first?.autoresizingMask = [.width, .height]
+		
+		return visualEffectView
 	}
 	
 	func updateNSView(_ nsView: NSView, context: Context) {}
 	
 	private let content: Content
+}
+
+private extension _ViewRepresentable {
+	final class HostingView<Content: View>: NSHostingView<Content> {
+		override var allowsVibrancy: Bool {
+			true
+		}
+	}
 }
 
 #endif
