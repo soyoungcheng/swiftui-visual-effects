38f4039f60a0dceb53a9d1489244a9023d033bff
Lucas Brown <lucasbrown@lucasbrownish.com>
2020-07-03 14:52:15 -0500

    Add content property

--- Sources/SwiftUIVisualEffects/Wrappers/macOS/ViewRepresentable.swift
+++ Sources/SwiftUIVisualEffects/Wrappers/macOS/ViewRepresentable.swift
@@ -7,13 +7,17 @@
 import SwiftUI
 
 struct _ViewRepresentable<Content: View>: NSViewRepresentable {
-	init(content: Content) {}
+	init(content: Content) {
+		self.content = content
+	}
 	
 	func makeNSView(context: Context) -> NSView {
 		NSView()
 	}
 	
 	func updateNSView(_ nsView: NSView, context: Context) {}
+	
+	private let content: Content
 }
 
 #endif
